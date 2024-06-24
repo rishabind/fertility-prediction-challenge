36066309b30be177c5bb5346aa09e6ad7311ea92
Tyagi <tyagi@demogr.mpg.de>
2024-06-24 10:34:27 +0200

    changes

--- submission.py
+++ submission.py
@@ -50,9 +50,11 @@ def clean_df(df, background_df=None):
                     ,"cf20m003", "cf20m030", "cf20m128","ci20m006","ci20m007"
                     ,"ci20m008", "ch20m002","cv20l041","cv20l043","cv20l044"] 
 
+    
+
 
     # Keeping data with variables selected
-    df = df[keepcols]
+    cleaned_df = df[keepcols]
 
     return cleaned_df
 
